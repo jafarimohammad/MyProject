1dde895e61bc3dd897bdbbdabc79fded964416d4
Mohammad Jafari <107628030+jafarimohammad@users.noreply.github.com>
2022-07-31 22:11:57 +0430

    Create bash.sh

---
+++ Bash-Script/bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+user=$(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 | sed ':a;N;$!ba;s/\n/ /g')
+notremove="ahanonline"
+for str in ${user[@]}; do
+  if [ "$str" != "$notremove" ]
+  then
+          deluser $str
+  fi
+done
