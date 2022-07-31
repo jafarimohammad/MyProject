3dccd43c3072e327994e89071804c7bd7d16e009
Mohammad Jafari <107628030+jafarimohammad@users.noreply.github.com>
2022-07-31 22:13:56 +0430

    Update bash.sh

--- Bash-Script/bash.sh
+++ Bash-Script/bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 user=$(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 | sed ':a;N;$!ba;s/\n/ /g')
-notremove="ahanonline"
+notremove="mohammad"
 for str in ${user[@]}; do
   if [ "$str" != "$notremove" ]
   then
